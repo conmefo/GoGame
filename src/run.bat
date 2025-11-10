d2fa469560b52fdf9c15b688edd7e7b18184f6e3
nghiale29-coder <nghialetrongdai2909@gmail.com>
2025-11-10 11:17:57 +0700

    Tinu edit

--- src/run.bat
+++ src/run.bat
@@ -1,7 +1,7 @@
 @echo off
 echo === Compiling SFML project ===
 g++ -c main.cpp -I"C:\SFML-2.6.2\include"
-g++ -L"C:\SFML-2.6.2\lib" main.o -o run.exe -lmingw32 -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-main -mwindows
+g++ -L"C:\SFML-2.6.2\lib" main.o -o GoGame.exe -lmingw32 -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-main -mwindows
 echo === Running program ===
-test.exe
+GoGame.exe
 pause
