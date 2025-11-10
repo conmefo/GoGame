141c60b4d835f2812806cb1bdb707a53627dfd21
conmefo <hoquangminh757@gmail.com>
2025-11-10 15:42:45 +0700

    add sfml 2.6.2

---
+++ build/CMakeFiles/GoGame.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/coding/project/GoGame/src/Board.cpp" "CMakeFiles/GoGame.dir/src/Board.cpp.obj" "gcc" "CMakeFiles/GoGame.dir/src/Board.cpp.obj.d"
+  "D:/coding/project/GoGame/src/Game.cpp" "CMakeFiles/GoGame.dir/src/Game.cpp.obj" "gcc" "CMakeFiles/GoGame.dir/src/Game.cpp.obj.d"
+  "D:/coding/project/GoGame/src/GameUI.cpp" "CMakeFiles/GoGame.dir/src/GameUI.cpp.obj" "gcc" "CMakeFiles/GoGame.dir/src/GameUI.cpp.obj.d"
+  "D:/coding/project/GoGame/src/GameView.cpp" "CMakeFiles/GoGame.dir/src/GameView.cpp.obj" "gcc" "CMakeFiles/GoGame.dir/src/GameView.cpp.obj.d"
+  "D:/coding/project/GoGame/src/main.cpp" "CMakeFiles/GoGame.dir/src/main.cpp.obj" "gcc" "CMakeFiles/GoGame.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
